62ad6aa6f9a15402c9af6ede1435a5991655373c
hk-pG <hukada.gogotea@gmail.com>
2025-03-17 20:22:39 +0900

    delete manjaro env written

--- environment.zsh
+++ environment.zsh
@@ -5,18 +5,6 @@ export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 # local go
 export PATH="$PATH:$HOME/go/bin"
 
-# aqua package manager
-export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
-
-# asdf
-. "$HOME/.asdf/asdf.sh"
-# append completions to fpath
-fpath=(${ASDF_DIR}/completions $fpath)
-# initialise completions with ZSH's compinit
-autoload -Uz compinit && compinit
-
-### local tools
-
 # sheldon
 export PATH="$PATH:$HOME/local_tools/sheldon/target/release"
 eval "$(sheldon source)"
